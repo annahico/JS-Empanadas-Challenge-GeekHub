9b6f13507f25d175b85a65b7822b7094a83f9e84
annahico <annahico@gmail.com>
2024-05-02 12:59:15 +0200

    añadiendo else if

--- main.js
+++ main.js
@@ -6,4 +6,12 @@ export const fn = (a, b, c) => {
    if (a < 0 || b < 0 || c < 0) {                                                
       throw new Error("Please enter a valid quantity of empanadas"); 
    }
+
+   else if ((a + b + c) >= 40) {                                                
+      throw new Error("Exceeds maximum quantity of empanadas");
+   }
+
+   else if ((a + b + c) % 3 != 0) {                                              
+      throw new Error("3x1 offer not applicable");
+   }
 }
